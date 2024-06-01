d1161b669787aed78b3cb5808f9ad5244c45ad04
Rahul Rameshbabu <rrameshbabu@nvidia.com>
2024-06-01 10:49:32 -0700

    globalprotect-openconnect: remove deprecated 1.x package
    The 1.x iteration of globalprotect-openconnect is no longer being
    developed. Remove related components from nixpkgs.
    
    Signed-off-by: Rahul Rameshbabu <sergeantsagara@protonmail.com>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1005,7 +1005,6 @@
   ./services/networking/gdomap.nix
   ./services/networking/ghostunnel.nix
   ./services/networking/git-daemon.nix
-  ./services/networking/globalprotect-vpn.nix
   ./services/networking/gns3-server.nix
   ./services/networking/gnunet.nix
   ./services/networking/go-autoconfig.nix
